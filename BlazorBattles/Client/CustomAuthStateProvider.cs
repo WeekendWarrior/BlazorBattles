47dfbb2ba8c67c89c4b82846e7e6f91bd2974816
Steven Striga <sstriga@gmail.com>
2021-04-16 09:25:12 -0400

    Update nav menu with authorizeView, added fav icon, updated app loading

--- BlazorBattles/Client/CustomAuthStateProvider.cs
+++ BlazorBattles/Client/CustomAuthStateProvider.cs
@@ -16,18 +16,20 @@ namespace BlazorBattles.Client
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            // Default the state to empty / unauthorized.
+            var state = new AuthenticationState(new ClaimsPrincipal());
+
+            // Check local storage for item "isAuthenticated" (this will be changed later to use tokens).
             if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
             {
                 var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "Steve") }, "test authentication type");
 
                 var user = new ClaimsPrincipal(identity);
 
-                var state = new AuthenticationState(user);
-
-                NotifyAuthenticationStateChanged(Task.FromResult(state));
-
-                return state;
+                state = new AuthenticationState(user);
             }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
 
             return new AuthenticationState(new ClaimsPrincipal());
         }
