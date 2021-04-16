ebf6dffa9aa62bda6461822a9d8de8cfcb8253c2
Steven Striga <sstriga@gmail.com>
2021-04-16 08:23:14 -0400

    Login & Register Forms

---
+++ BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBattles.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage ="Please enter a Username.")]
+        public string Username { get; set; }
+
+        [Required] 
+        public string Password { get; set; }
+    }
+}
---
+++ BlazorBattles/Shared/UserRegister.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBattles.Shared
+{
+    public class UserRegister
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(16, ErrorMessage = "Your Username is too long (16 characters max)")]
+        public string Username { get; set; }
+
+        public string Bio { get; set; }
+
+        [Required, StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "The passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Please choose a number between 0 and 1,000")]
+        public int Bananas { get; set; } = 100;
+
+        public string StartUnitId { get; set; } = "1"; // Knight
+
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Only confirmed users can play!")]
+        public bool IsConfirmed { get; set; } = true;
+    }
+}
