02117e0c6128256d838de1a15b96180f54d58626
Steven Striga <sstriga@gmail.com>
2021-04-17 09:53:31 -0400

    Added post, put, delete web api methods to unit controller

--- BlazorBattles/Server/Controllers/UnitController.cs
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -27,12 +27,48 @@ namespace BlazorBattles.Server.Controllers
             return Ok(units);
         }
 
-        //[HttpGet]
-        //public IActionResult GetUnits()
-        //{
-        //    _unitService.LoadUnitsAsync();
+        [HttpPost]
+        public async Task<IActionResult> AddUnit(Unit unit)
+        {
+            await _context.Units.AddAsync(unit);
+            await _context.SaveChangesAsync();
 
-        //    return Ok(_unitService.Units);
-        //}
+            var updatedUnits = await _context.Units.ToArrayAsync();
+
+            return Ok(updatedUnits);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUnit(int id, Unit unit)
+        {
+            Unit dbUnit = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (dbUnit == null) return NotFound($"Unit with id {id} does not exist.");
+
+            dbUnit.Title = unit.Title;
+            dbUnit.Attack = unit.Attack;
+            dbUnit.Defense = unit.Defense;
+            dbUnit.BananaCost = unit.BananaCost;
+            dbUnit.HitPoints = unit.HitPoints;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(dbUnit);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            Unit dbUnit = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (dbUnit == null) return NotFound($"Unit with id {id} does not exist.");
+
+            _context.Units.Remove(dbUnit);
+            await _context.SaveChangesAsync();
+
+            var updatedUnits = await _context.Units.ToArrayAsync();
+
+            return Ok(updatedUnits);
+        }
     }
 }
