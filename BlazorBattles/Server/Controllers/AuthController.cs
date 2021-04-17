4467dc746ded78b16abb582a48fc51e620face47
Steven Striga <sstriga@gmail.com>
2021-04-17 10:49:53 -0400

    Added Auth Repository and WebApi Controller

---
+++ BlazorBattles/Server/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using BlazorBattles.Server.Data;
+using BlazorBattles.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegister request)
+        {
+            var user = new User
+            {
+                Username = request.Username,
+                Email = request.Email,
+                Bananas = request.Bananas,
+                DateOfBirth = request.DateOfBirth,
+                IsConfirmed = request.IsConfirmed
+            };
+
+            var response = await _authRepository.Register(user, request.Password);
+
+            if (!response.Success) return BadRequest(response);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ BlazorBattles/Server/Data/AuthRepository.cs
@@ -0,0 +1,64 @@
+﻿using BlazorBattles.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<int>
+                {
+                    Message = "User already exists."
+                };
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int>
+            {
+                Data = user.Id,
+                Success = true,
+                Message = "Registration successful!"
+            };
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            var userExists = await _context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+
+            return userExists;
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
