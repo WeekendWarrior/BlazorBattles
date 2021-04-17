9bad45b438cbab41427c9a576d9cb5301f462c9a
Steven Striga <sstriga@gmail.com>
2021-04-17 15:44:59 -0400

    Added AddBananas endpoint to UserController and now persisting the banana count in the database

--- BlazorBattles/Client/Services/BananaService.cs
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -27,10 +27,10 @@ namespace BlazorBattles.Client.Services
 
         void BananasChanged() => OnChange.Invoke();
 
-        public void AddBananas(int amount)
+        public async Task AddBananas(int amount)
         {
-            Bananas += amount;
-
+            var result = await _http.PutAsJsonAsync("api/User/AddBananas", amount);
+            Bananas = await result.Content.ReadFromJsonAsync<int>();
             BananasChanged();
         }
 
