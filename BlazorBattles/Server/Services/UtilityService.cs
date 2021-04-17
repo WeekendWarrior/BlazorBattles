9f0caf39c9e618fdf0efac0089f1ca8b423465d1
Steven Striga <sstriga@gmail.com>
2021-04-17 16:40:10 -0400

    Created UserUnitController & BuildUserUnit endpoint

--- BlazorBattles/Server/Services/UtilityService.cs
+++ BlazorBattles/Server/Services/UtilityService.cs
@@ -2,6 +2,8 @@
 using BlazorBattles.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Security.Authentication;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -20,7 +22,12 @@ namespace BlazorBattles.Server.Services
 
         public async Task<User> GetUser()
         {
-            var userId = int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var claimNameId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (claimNameId == null) 
+                throw new AuthenticationException($"Could not find claim '{ClaimTypes.NameIdentifier}' for user: {_httpContextAccessor.HttpContext.User.Identity.Name}");
+
+            var userId = int.Parse(claimNameId);
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
             
             return user;
